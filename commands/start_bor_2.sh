5817d8fa9d27fe1cec2cd12e309ba0487c6297f5
tosettil-polimi <info@tosettil.me>
2025-01-30 16:37:36 +0100

    fix(start_bor): params

--- commands/start_bor_2.sh
+++ commands/start_bor_2.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-docker run -d \
-  -v $(pwd)/erigon_data:/data/bor \
-  -p 3133:3131 \
-  -p 8546:8545 \
-  -p 30304:30303 \
-  bor-dev-local \
-  server -chain /data/bor/genesis.json \
-    --datadir /data/bor/datadir_2 \
-    --bor.heimdall=http://host.docker.internal:1318 \
-    --mine \
-    --miner.etherbase="0xf5e55a3e66d028c59c12185e2e51cdafe2c53baf" \
-    --allow-insecure-unlock \
-    --bor.withoutheimdall=false \
-    --bor.heimdallgRPC "host.docker.internal:3134" \
-    --bor.logs true \
-    --password /data/bor/password.txt \
-    --port 30303 \
-    --http --http.addr "localhost" --http.port "8546" \
-    --http.api "eth,net,web3,txpool,debug,bor" \
-    --http.corsdomain "*" --http.vhosts "*" \
-    --bootnodes "enode://d25526ab1a2e372fd136cae3fb678deea069639436d34ca0cbb2f80629d43bfd3c2f8fdd5c4940380b18e771199bdf577bfd984e85df816e9c00dbd25e7839ec@host.docker.internal:30303"
+./bin/bor server -chain ./erigon_data/genesis.json \
+  --datadir ./erigon_data/bor_datadir_2 \
+  --bor.heimdall=http://localhost:1318 \
+  --mine \
+  --miner.etherbase="0xf5e55a3e66d028c59c12185e2e51cdafe2c53baf" \
+  --allow-insecure-unlock \
+  --bor.withoutheimdall=false \
+  --bor.heimdallgRPC "localhost:3134" \
+  --port 30304 \
+	--grpc.addr :3133 \
+  --bor.logs true \
+  --http --http.addr "localhost" --http.port "8546" \
+  --http.api "eth,net,web3,txpool,debug,bor" \
+  --http.corsdomain "*" --http.vhosts "*" \
+  --bootnodes "enode://d25526ab1a2e372fd136cae3fb678deea069639436d34ca0cbb2f80629d43bfd3c2f8fdd5c4940380b18e771199bdf577bfd984e85df816e9c00dbd25e7839ec@127.0.0.1:30303"
+  --password ./erigon_data/password.txt \
 
 exit 0
